a935d6e864efa01455a4a8b58efd115df8471041
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-07-21 14:19:29 +0600

    feat: add autoparts search class

---
+++ api.py
@@ -0,0 +1,24 @@
+from typing import Dict, List
+import requests
+import settings
+
+
+class AutopartsSearch:
+    def __init__(self):
+        self.host = f'https://{settings.settings.get("ABCP", "HOST")}'
+        self.user = settings.settings.get('ABCP', 'USER')
+        self.md5p = settings.settings.get('ABCP', 'MD5P')
+
+    @property
+    def auth_credits(self) -> Dict[str, str]:
+        return {'userlogin': self.user, 
+                'userpsw': self.md5p}
+
+    def get_autoparts_from(self, vendor_code: str) -> List[Dict[str, str | int]]:
+        params = {**self.auth_credits, **{'number': vendor_code}}
+        data = requests.get(url=f'{self.host}/search/brands/', params=params).json()
+        return list(data.values())
+
+    def get_brands_from(self, vendor_code: str) -> List[str]:
+        data = self.get_autoparts_from(vendor_code=vendor_code)
+        return [autopart['brand'] for autopart in data]
