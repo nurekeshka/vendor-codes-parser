1cb22461b52eb182b69d5998ed3cb1869da53c88
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2023-07-21 12:55:34 +0600

    fix: divide generator to serializer and excel classes

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-from service import Generator
+from serializers import Serializer
+from generator import Generator
+from excel import Excel
 import sys
 
 
 def main():
     try:
-        generator = Generator(sys.argv[1])
+        excel = Excel(sys.argv[1])
     except IndexError as exception:
         raise ValueError('Import filename was not provided in system args.') from exception
+
+    serializer = Serializer(excel.rows)
+    generator = Generator(serializer.data)
+
     try:
         generator.export(sys.argv[2])
     except IndexError as exception:
